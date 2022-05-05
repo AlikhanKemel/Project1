7ce4b86ea6d075d8aaf4a08ef17797f245b4c6b4
alikh <alikh@DESKTOP-06RJ92H>
2022-05-06 03:09:18 +0600

    Добавьте файлы проекта.

---
+++ Project1/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Project1.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Project1/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class Ticket
+    {     
+        [Key]
+        public int TicketId { get; set; }
+        [Required]
+        public string Name { get; set;}
+        [Required]
+        public string CardNUmber { get; set;}
+
+         
+    }
+}
